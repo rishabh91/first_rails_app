43acd6d378de8d682eb6b7abd79e0bf98d7a82ed
Rishabh <rishabh@freshdesk.com>
2016-05-20 15:14:47 +0530

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ActiveRecord::Base
+	before_save { self.email = email.downcase }
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :name, presence: true, length: {maximum: 50 }
+	validates :email, presence: true, length: {maximum: 100 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false } 
+	has_secure_password
+	validates :password, presence: true, length: { minimum: 1 }
+	def User.digest(string)
+	    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+	                                                  BCrypt::Engine.cost
+	    BCrypt::Password.create(string, cost: cost)
+  end
+end
